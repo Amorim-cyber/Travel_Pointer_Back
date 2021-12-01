e0a931fc8768e82cf7664656f8e8a0ba5e2db31b
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-12-01 16:27:28 -0300

    Adiciona Dto[1]

---
+++ src/main/java/com/app/travelPointer/model/dto/LocalDTO.java
@@ -0,0 +1,82 @@
+package com.app.travelPointer.model.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class LocalDTO {
+
+    private Long id;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    private String endereco;
+
+    @NotNull
+    private String latitude;
+
+    @NotNull
+    private String longitude;
+
+    @NotNull
+    private String tipo;
+
+    @NotNull
+    private Long idUsuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
