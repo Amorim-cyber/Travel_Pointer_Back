ff3d83b382ff8450422bf4c5b5b382e7f219ffa9
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-12-01 15:03:32 -0300

    Adiciona Util

---
+++ src/main/java/com/app/travelPointer/util/MessageUtils.java
@@ -0,0 +1,11 @@
+package com.app.travelPointer.util;
+
+public abstract class MessageUtils {
+
+    public static final String LOCAL_ALREADY_EXISTS = "Local already register in the database";
+
+    public static final String USER_ALREADY_EXISTS = "User already exists in the database";
+
+    public static final String NO_RECORDS_FOUND = "No records found";
+
+}
